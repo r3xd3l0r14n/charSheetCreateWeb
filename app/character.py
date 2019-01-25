398595203aff3bdef70b00341ae5e33250362bdf
Colin Delorme <re>
2019-01-25 14:12:01 -0500

    v0.6 Tons of additions here, Operative page added, skills page final pass, full rework of the pdf print feature

--- app/character.py
+++ app/character.py
@@ -1,11 +1,15 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
-import io, random
+import io, random, os, json
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 
 
-class Character:
-    cls = ()
+cwd = os.getcwd()
+
+with open(cwd+'/app/json/skills.json', 'rb') as f:
+    mSkills = json.load(f)
+
+class Character():
     attrRoll = []
     attribs = {}
     atribMod = {}
@@ -13,19 +17,15 @@ class Character:
               "10": "0", "11": "0", "12": "1", "13": "1", "14": "2", "15": "2", "16": "3", "17": "3", "18": "4",
               "19": "4", "20": "5"}
 
-    def setNameRace(self, name, race, gender):
-        self.name = name
-        self.race = race
-        self.gender = gender
-
-    def setTheme(self, theme):
-        self.theme = theme
-
-    def setClass(self, cls):
-        self.cls = cls
-
-    def setImprovs(self, v):
-        self.improvs = v
+    # def __init__(self, name=None, race=None, gender=None, theme=None, cls=None, improvs=None, attribs=None, atribMod=None):
+    #     self.race = race
+    #     self.name = name
+    #     self.gender = gender
+    #     self.theme = theme
+    #     self.cls = cls
+    #     self.improvs = improvs
+    #     self.attribs = attribs
+    #     self.atribMod = atribMod
 
     def rollAtr(self):
         x = 0
@@ -39,7 +39,6 @@ class Character:
             attrDict.append(sum(i))
             x += 1
         self.attrRoll = attrDict
-        print(self.attrRoll)
         return attrDict
 
     def checkAttribs(self, data):
@@ -79,27 +78,61 @@ class Character:
                     self.attribs[k] = v
                     self.atribMod[k] = self.abMods[v]
 
-    def setSkills(self, data):
+    def setSkills(self, data, cls):
         self.skills = data
-        # Will buff this up more later as we get closer to actually building our PDF
+        with open(cwd+'/app/json/classes/'+cls.lower()+'.json', 'rb') as f:
+            clsFile = json.load(f)
+        self.sRanks = int(clsFile['sRanks'])+int(self.atribMod[clsFile['sRanksABM']])
+        return self.sRanks
         #TODO - Finish this section for final CRD 12/24/2018
 
-    def createPDF(self):
+    def createPDF(self,ses):
         packet = io.BytesIO()
         # create a new PDF with Reportlab
         can = canvas.Canvas(packet, pagesize=letter)
-        can.drawString(155, 753, self.name)  # Draw the name on the top
-        can.drawString(215, 731, self.race)  # Draw our character race on the sheet
-        can.drawString(165, 710, self.gender)  # Draw gender
-        can.drawString(20, 730, self.cls)  # Draw Class
-        can.drawString(315, 730, self.theme)  # Theme
+        can.drawString(155, 753, ses['char'])  # Draw the name on the top
+        can.drawString(215, 731, ses['race'])  # Draw our character race on the sheet
+        can.drawString(165, 710, ses['gender'])  # Draw gender
+        can.drawString(20, 730,  ses['class'])  # Draw Class
+        can.drawString(315, 730, ses['theme'])  # Theme
+        can.drawString(256.5, 483, str(ses['sRanks']))  # Draw skill levels
+        # Draw ability scores
+        can.drawString(95, 620, ses['attr']['str'])  # Str
+        can.drawString(142, 620, self.abMods[ses['attr']['str']])  # Str Mod
+        can.drawString(95, 599, ses['attr']['dex'])  # DEX
+        can.drawString(142, 599, self.abMods[ses['attr']['dex']])  # DEX Mod
+        can.drawString(95, 578, ses['attr']['con'])  # CON
+        can.drawString(142, 578, self.abMods[ses['attr']['con']])  # CON Mod
+        can.drawString(95, 556, ses['attr']['int'])  # INT
+        can.drawString(142, 556, self.abMods[ses['attr']['int']])  # INT Mod
+        can.drawString(95, 535, ses['attr']['wis'])  # WIS
+        can.drawString(142, 535, self.abMods[ses['attr']['wis']])  # WIS Mod
+        can.drawString(95, 514, ses['attr']['cha'])  # CHA
+        can.drawString(142, 514, self.abMods[ses['attr']['cha']])  # CHA Mod
+        # This draws all most if not all of our class skill related data on the sheet
+        for s in mSkills:
+            if ses['class'].lower() in mSkills[s]['class']:
+                x = float(mSkills[s]["pos"]["x"])
+                y = float(mSkills[s]["pos"]["y"])
+                can.drawImage('10003.png', x, y, 10, 8,
+                              mask=[255, 255, 255, 255, 255, 255])
+            if mSkills[s]['name'] in ses['skills']:
+                nm = mSkills[s]
+                # Total First
+                can.drawString(nm['pos']['xTot'], nm['pos']['yTot'], str(ses['skills'][nm['name']]['tot']))
+                # Ranks
+                can.drawString(nm['pos']['xRnk'], nm['pos']['yRnk'], ses['skills'][nm['name']]['rnk'])
+                # Bonus
+                can.drawString(nm['pos']['xBon'], nm['pos']['yBon'], ses['skills'][nm['name']]['bon'])
+                # Ability Mod
+                can.drawString(nm['pos']['xAbm'], nm['pos']['yAbm'], ses['skills'][nm['name']]['abm'])
         can.save()
 
         # move to the beginning of the StringIO buffer
         packet.seek(0)
         new_pdf = PdfFileReader(packet)
         # read your existing PDF
-        existing_pdf = PdfFileReader(open("../original.pdf", "rb"))
+        existing_pdf = PdfFileReader(open(cwd+"/app/original.pdf", "rb"))
         output = PdfFileWriter()
         # add the "watermark" (which is the new pdf) on the existing page
         page = existing_pdf.getPage(0)
@@ -109,10 +142,14 @@ class Character:
         output.addPage(page2)
         # finally, write "output" to a real file
         # TODO - This needs to be fixed such that it won't create a file with the same name CRD 12/27/18
-        outputStream = open("../destination.pdf", "wb")
+        outputStream = open(cwd+"/app/destination.pdf", "wb")
         output.write(outputStream)
         outputStream.close()
 
+### toJson Method
+    def toJson(self):
+        finJ = {"charN":self.name}
+        return finJ
 
 
 
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
-from flask import render_template, send_file, redirect, url_for, jsonify, request
+from flask import render_template, send_file, redirect, url_for, jsonify, request, session
 from app import app
 from app.forms import *
 from app.character import Character
 import json, os
+
+app.secret_key = "asd93j2jf04kv93vn1nvjd2et"
 
 char = Character()
 cwd = os.getcwd()
@@ -18,9 +20,9 @@ def index():
 def charN():
     form = charNForm()
     if form.validate_on_submit():
-        char.setNameRace(form.charN.data, form.race.data, form.gender.data)
-        # char.createPDF()
-        # return redirect(url_for('show_static_pdf'))
+        session['char'] = form.charN.data
+        session['race'] = form.race.data
+        session['gender'] = form.gender.data
         return redirect(url_for('charTheme'))
     return render_template('charStart.html', title="Create new Char", form=form)
 
@@ -29,79 +31,92 @@ def charN():
 def charTheme():
     form = charThemeForm()
     if form.validate_on_submit():
-        char.setTheme(form.theme.data)
+        print(session)
+        session['theme'] = form.theme.data
         return redirect(url_for('charClass'))
     return render_template('charTheme.html', title="Select Theme and Level", form=form)
 
 
 @app.route('/charClass', methods=['GET', 'POST'])
 def charClass():
-    # form = charClassForm()
-    # if form.validate_on_submit():
-    #     return redirect(url_for('sel_char'))
     if request.method == 'POST':
-        n = char.cls
-        s = 'charSel'+n
+        n = session['class']
+        s = 'charSel' + n
         return redirect(url_for(s))
     return render_template('charClass.html', title="Pick A Class")
 
-#Character Ability Description and Selection Screens
+
+# Character Ability Description and Selection Screens
 # TODO - return to this to make it more concise 11/27/2018 CRD
 @app.route('/charSelectionEnvoy', methods=['GET', 'POST'])
 def charSelEnvoy():
     if request.method == 'POST':
         v = request.form['sel1']
-        char.setImprovs(v)
+        session['improvs'] = v
         return redirect(url_for('rollAttrib'))
     return render_template('classSel/charSelEnvoy.html', title="Envoy")
+
 
 @app.route('/charSelectionMechanic')
 def charSelMechanic():
     return render_template('classSel/charSelMechanic.html', title="Mechanic")
 
+
 @app.route('/charSelectionSolarian')
 def charSelSolarian():
     return render_template('classSel/charSelSolarian.html', title="Solarian")
+
 
 @app.route('/charSelectionMystic')
 def charSelMystic():
     return render_template('classSel/charSelMystic.html', title="Mystic")
 
+
 @app.route('/charSelectionTechnomancer')
 def charSelTechnomancer():
     return render_template('classSel/charSelTechnomancer.html', title="Technomancer")
+
 
 @app.route('/charSelectionSoldier')
 def charSelSoldier():
     return render_template('classSel/charSelSoldier.html', title="Soldier")
 
-@app.route('/charSelectionOperative')
+
+@app.route('/charSelectionOperative', methods=['GET', 'POST'])
 def charSelOperative():
+    if request.method == 'POST':
+        v = request.form['sel1']
+        session['ospecs'] = v
+        return redirect(url_for('rollAttrib'))
     return render_template('classSel/charSelOperative.html', title="Operative")
 
-#Rolling Attributes screen
-@app.route('/rollAttrib', methods=['GET','POST'])
+
+# Rolling Attributes screen
+@app.route('/rollAttrib', methods=['GET', 'POST'])
 def rollAttrib():
     if request.method == 'POST':
         return redirect(url_for("selSkills"))
     return render_template('rollAttrib.html', title='Roll Attributes')
 
+
 @app.route('/selSkills', methods=['GET', 'POST'])
 def selSkills():
     if request.method == 'POST':
-        char.createPDF()
-        return redirect(url_for("feats")) #TODO - Change this back later
+        char.createPDF(session)
+        #return redirect(url_for("feats"))  # TODO - Change this back later
+        return redirect(url_for('show_static_pdf'))
     return render_template('selSkills.html', title='Select Skills')
+
 
 @app.route('/feats', methods=['GET', 'POST'])
 def feats():
     return render_template('feats.html', title='Feats')
 
 
-#Final Screen to show the pdf Files
+# Final Screen to show the pdf Files
 @app.route('/show/static-pdf')
 def show_static_pdf():
-    return send_file('../destination.pdf', mimetype='application/pdf', attachment_filename='file.pdf')
+    return send_file(cwd+'/app/destination.pdf', mimetype='application/pdf', attachment_filename='file.pdf')
 
 
 ###################################
@@ -109,7 +124,7 @@ def show_static_pdf():
 ###################################
 @app.route('/get_theme/<theme>')
 def get_theme(theme):
-    with open(cwd+'/app/json/theme.json', 'rb') as file:
+    with open(cwd + '/app/json/theme.json', 'rb') as file:
         themes = json.load(file)
     if theme in themes:
         print(jsonify(themes[theme]))
@@ -121,43 +136,51 @@ def get_theme(theme):
 @app.route('/get_class/<charCls>')
 def get_class(charCls):
     charClsJson = cwd + '/app/json/classes/' + charCls.lower() + '.json'
-    char.setClass(charCls)
+    session['class'] = charCls
     with open(charClsJson, 'rb') as file:
         charClsFile = json.load(file)
-    if charCls == "Envoy":
-        return jsonify(charClsFile)
-    elif charClsFile[charCls]:
-        return jsonify(charClsFile)
+    return jsonify(charClsFile)
+
+
 
 @app.route('/get_attrib')
 def getAttrib():
-    with open(cwd+'/app/json/attrib.json', 'rb') as f:
+    with open(cwd + '/app/json/attrib.json', 'rb') as f:
         attribJSON = json.load(f)
     attribJSON['roll'] = char.rollAtr()
     return jsonify(attribJSON)
 
+
 @app.route('/getSkills')
 def getSkills():
-    with open(cwd +'/app/json/skills.json', 'rb') as f:
+    with open(cwd + '/app/json/skills.json', 'rb') as f:
         skillJSON = json.load(f)
-    skillJSON['class'] = char.cls.lower()
-    with open(cwd+'/app/json/classes/'+skillJSON['class'].lower()+'.json', 'rb') as f:
+    skillJSON['class'] = session['class'].lower()
+    with open(cwd + '/app/json/classes/' + skillJSON['class'].lower() + '.json', 'rb') as f:
         cls = json.load(f)
-    ABM = char.abMods[char.attribs[cls['sRanksABM']]]
+    ABM = char.abMods[session['attr'][cls['sRanksABM']]]
     ranks = cls['sRanks']
-    skillJSON['ranks'] = int(ABM)+int(ranks)
+    skillJSON['ranks'] = int(ABM) + int(ranks)
     skillJSON['abMod'] = char.atribMod
     return jsonify(skillJSON)
 
+
 @app.route('/setAttrib', methods=['GET', 'POST'])
 def setAttrib():
-    data = request.json
-    char.checkAttribs(data) #This sets the skills at the same time
+    session['attr'] = request.json
+    char.checkAttribs(session['attr'])  # This sets the skills at the same time
     return "Got the message"
+
 
 @app.route('/setSkills', methods=['GET', 'POST'])
 def setSkill():
     data = request.json
-    char.setSkills(data)
+    session['skills'] = data
+    session['sRanks'] = char.setSkills(data, session['class'])
     return "Got the message"
 
+
+@app.route('/killSession')
+def killSession():
+    session.pop('username', None)
+    return redirect(url_for('index'))
